d81614e7d7a7c27f05ff7d96d25ea64e9422788d
wiksien <88692323+wiksien@users.noreply.github.com>
2024-07-12 00:13:20 +0200

    configured CORS

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,14 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import Script from "next/script";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Cat Picture Daily",
-  description: "Discover a new cat picture every day. Visit for your daily dose of feline cuteness and brighten up your day with charming cat photos!",
+  description:
+    "Discover a new cat picture every day. Visit for your daily dose of feline cuteness and brighten up your day with charming cat photos!",
 };
 
 export default function RootLayout({
@@ -16,10 +19,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <script defer src="http://umami-c80wsck.5.161.97.168.sslip.io/script.js" data-website-id="ee66bdf5-3765-4121-8d5f-485fac9a6207"></script>
-      </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          defer
+          src="https://umami.catpicturedaily.com/script.js"
+          data-website-id="ee66bdf5-3765-4121-8d5f-485fac9a6207"
+        />
+        {children}
+      </body>
     </html>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
   //images: { unoptimized: true },
   // ... rest of the configuration.
 
@@ -10,6 +9,28 @@ const nextConfig = {
   //Static setup
   //output: "export",
   //assetPrefix: "cdn host",
+
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://catpicturedaily.com" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
